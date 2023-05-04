80500b6428971b22e3c6fdc825da71f86832a017
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-05-04 21:35:47 +0200

    Add media client initialization and error response struct to root controller

--- internal/controllers/root.go
+++ internal/controllers/root.go
@@ -2,9 +2,16 @@ package controllers
 
 import (
 	"github.com/bingemate/media-service/initializers"
+	"github.com/bingemate/media-service/pkg"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
+type errorResponse struct {
+	Error string `json:"error"`
+}
+
 func InitRouter(engine *gin.Engine, db *gorm.DB, env initializers.Env) {
+	var mediaClient = pkg.NewMediaClient(env.TMDBApiKey)
+	InitMediaDataController(engine.Group("/media"), mediaClient)
 }
