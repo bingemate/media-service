b2a388782381b0e176afa207f540f93d73135381
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-05-07 20:04:34 +0200

    Add methods to retrieve media and media file information in MediaRepository

--- internal/repository/media.go
+++ internal/repository/media.go
@@ -40,3 +40,51 @@ func (r *MediaRepository) GetMediaRating(tmdbID int) (float32, int, error) {
 	}
 	return sum / float32(count), count, nil
 }
+
+func (r *MediaRepository) GetMedia(id string) (*repository.Media, error) {
+	var media repository.Media
+	err := r.db.Where("id = ?", id).First(&media).Error
+	if err != nil {
+		return nil, err
+	}
+	return &media, nil
+}
+
+func (r *MediaRepository) GetMediaByTmdbID(tmdbID int) (*repository.Media, error) {
+	var media repository.Media
+	err := r.db.Where("tmdb_id = ?", tmdbID).First(&media).Error
+	if err != nil {
+		return nil, err
+	}
+	return &media, nil
+}
+
+func (r *MediaRepository) GetEpisodeFileInfo(mediaID string) (*repository.MediaFile, error) {
+	var mediaFile repository.Episode
+	err := r.db.
+		Joins("Media").
+		Joins("MediaFile").
+		Preload("MediaFile.Audio").
+		Preload("MediaFile.Subtitles").
+		Where("media_id = ?", mediaID).
+		First(&mediaFile).Error
+	if err != nil {
+		return nil, err
+	}
+	return &mediaFile.MediaFile, nil
+}
+
+func (r *MediaRepository) GetMovieFileInfo(mediaID string) (*repository.MediaFile, error) {
+	var mediaFile repository.Movie
+	err := r.db.
+		Joins("Media").
+		Joins("MediaFile").
+		Preload("MediaFile.Audio").
+		Preload("MediaFile.Subtitles").
+		Where("media_id = ?", mediaID).
+		First(&mediaFile).Error
+	if err != nil {
+		return nil, err
+	}
+	return &mediaFile.MediaFile, nil
+}
