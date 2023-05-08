e822a48189c9e048d618679a61dd2bbd2b37f0bb
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-05-08 16:54:23 +0200

    Add functions to get TV show and episode info by TMDB ID and season episodes by TMDB ID to MediaData

--- internal/features/media.go
+++ internal/features/media.go
@@ -64,5 +64,84 @@ func (m *MediaData) GetMovieInfo(mediaID string) (*tmdb.Movie, error) {
 		}
 		return nil, err
 	}
+	if media.MediaType != repository2.MediaTypeMovie {
+		return nil, ErrInvalidMediaType
+	}
 	return m.GetMovieInfoByTMDB(media.TmdbID)
+}
+
+func (m *MediaData) GetEpisodeInfoByTMDB(tvTmdbID, season, episodeNumber int) (*tmdb.TVEpisode, error) {
+	episode, err := m.mediaClient.GetTVEpisode(tvTmdbID, season, episodeNumber)
+	if err != nil {
+		return nil, err
+	}
+	return episode, nil
+}
+
+func (m *MediaData) GetEpisodeInfo(mediaID string) (*tmdb.TVEpisode, error) {
+	media, err := m.mediaRepository.GetMedia(mediaID)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, ErrMediaNotFound
+		}
+		return nil, err
+	}
+	if media.MediaType != repository2.MediaTypeEpisode {
+		return nil, ErrInvalidMediaType
+	}
+	episode, err := m.mediaRepository.GetEpisode(mediaID)
+	if err != nil {
+		return nil, err
+	}
+	return m.GetEpisodeInfoByTMDB(episode.TvShow.Media.TmdbID, episode.NbSeason, episode.NbEpisode)
+}
+
+func (m *MediaData) GetTvShowInfoByTMDB(tmdbID int) (*tmdb.TVShow, error) {
+	tvShow, err := m.mediaClient.GetTVShow(tmdbID)
+	if err != nil {
+		return nil, err
+	}
+	voteAverage, voteCount, err := m.mediaRepository.GetMediaRating(tmdbID)
+	if err == nil {
+		tvShow.VoteAverage = voteAverage
+		tvShow.VoteCount = voteCount
+	}
+	return tvShow, nil
+}
+
+func (m *MediaData) GetTvShowInfo(mediaID string) (*tmdb.TVShow, error) {
+	media, err := m.mediaRepository.GetMedia(mediaID)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, ErrMediaNotFound
+		}
+		return nil, err
+	}
+	if media.MediaType != repository2.MediaTypeTvShow {
+		return nil, ErrInvalidMediaType
+	}
+	return m.GetTvShowInfoByTMDB(media.TmdbID)
+}
+
+func (m *MediaData) GetSeasonEpisodesByTMDB(tvTmdbID, season int) (*[]tmdb.TVEpisode, error) {
+	episodes, err := m.mediaClient.GetTVSeasonEpisodes(tvTmdbID, season)
+	if err != nil {
+		return nil, err
+	}
+	return episodes, nil
+}
+
+func (m *MediaData) GetSeasonEpisodes(tvMediaID string, season int) (*[]tmdb.TVEpisode, error) {
+	media, err := m.mediaRepository.GetMedia(tvMediaID)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, ErrMediaNotFound
+		}
+		return nil, err
+	}
+	if media.MediaType != repository2.MediaTypeTvShow {
+		return nil, ErrInvalidMediaType
+	}
+
+	return m.GetSeasonEpisodesByTMDB(media.TmdbID, season)
 }
