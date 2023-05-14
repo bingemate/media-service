52f49741148b57d373bac9492abc886e6d3c9e52
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-05-14 12:14:50 +0200

    feat: Add endpoint to get media file info by TMDB ID
    This commit adds a new GET endpoint to retrieve media file information by TMDB ID. The new endpoint is located at `/media-file/tmdb/:id`. Additionally, the `getMediaByTmdbID` function was added to the `MediaFile` struct in `media_files.go`.

--- internal/controllers/fileinfo.go
+++ internal/controllers/fileinfo.go
@@ -4,11 +4,15 @@ import (
 	"errors"
 	"github.com/bingemate/media-service/internal/features"
 	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
 func InitFileInfoController(engine *gin.RouterGroup, fileInfo *features.MediaFile) {
-	engine.GET(":id", func(c *gin.Context) {
+	engine.GET("file/:id", func(c *gin.Context) {
 		getMediaFileInfo(c, fileInfo)
+	})
+	engine.GET("file-tmdb/:id", func(c *gin.Context) {
+		getMediaFileInfoTmdb(c, fileInfo)
 	})
 }
 
@@ -25,6 +29,40 @@ func InitFileInfoController(engine *gin.RouterGroup, fileInfo *features.MediaFil
 func getMediaFileInfo(c *gin.Context, mediaData *features.MediaFile) {
 	id := c.Param("id")
 	result, err := mediaData.GetMediaFileInfo(id)
+	if err != nil {
+		if errors.Is(err, features.ErrMediaNotFound) {
+			c.JSON(404, errorResponse{
+				Error: err.Error(),
+			})
+			return
+		}
+		if errors.Is(err, features.ErrInvalidMediaType) {
+			c.JSON(400, errorResponse{
+				Error: err.Error(),
+			})
+			return
+		}
+		c.JSON(500, errorResponse{
+			Error: err.Error(),
+		})
+		return
+	}
+	c.JSON(200, toMediaFileResponse(result))
+}
+
+// @Summary		Get media file info by TMDB ID
+// @Description	Get media file info by TMDB ID
+// @Tags			Media File
+// @Param			id path int true "TMDB ID"
+// @Produce		json
+// @Success		200	{object} mediaFileResponse
+// @Failure		400	{object} errorResponse
+// @Failure		404	{object} errorResponse
+// @Failure		500	{object} errorResponse
+// @Router			/media-file/tmdb/{id} [get]
+func getMediaFileInfoTmdb(c *gin.Context, mediaData *features.MediaFile) {
+	tmdbID, err := strconv.Atoi(c.Param("id"))
+	result, err := mediaData.GetMediaByTmdbID(tmdbID)
 	if err != nil {
 		if errors.Is(err, features.ErrMediaNotFound) {
 			c.JSON(404, errorResponse{
