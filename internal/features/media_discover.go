5fcb9aa92e1a42169ae3b8594e2a98e9736fc13b
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-05-27 17:17:09 +0200

    feat: Add search functionality for available movies
    This commit adds a new function to search for available movies and modifies the existing functions to accept an additional boolean parameter. The new function searches for movies in the local repository and fetches their details from TMDB API concurrently. It also calculates the total number of pages based on the total results returned by TMDB API.

--- internal/features/media_discover.go
+++ internal/features/media_discover.go
@@ -1,8 +1,12 @@
 package features
 
 import (
+	repository2 "github.com/bingemate/media-go-pkg/repository"
 	"github.com/bingemate/media-go-pkg/tmdb"
 	"github.com/bingemate/media-service/internal/repository"
+	"log"
+	"math"
+	"sync"
 )
 
 type MediaDiscovery struct {
@@ -17,7 +21,15 @@ func NewMediaDiscovery(mediaClient tmdb.MediaClient, mediaRepository *repository
 	}
 }
 
-func (m *MediaDiscovery) SearchMovie(query string, page int) (*tmdb.PaginatedMovieResults, *[]bool, error) {
+func (m *MediaDiscovery) SearchMovie(query string, page int, available bool) (*tmdb.PaginatedMovieResults, *[]bool, error) {
+	movies, presence, err := m.searchAllMovie(query, page)
+	if err != nil {
+		return nil, nil, err
+	}
+	return movies, presence, nil
+}
+
+func (m *MediaDiscovery) searchAllMovie(query string, page int) (*tmdb.PaginatedMovieResults, *[]bool, error) {
 	movies, err := m.mediaClient.SearchMovies(query, page)
 	presence := make([]bool, len(movies.Results))
 	if err != nil {
@@ -34,7 +46,44 @@ func (m *MediaDiscovery) SearchMovie(query string, page int) (*tmdb.PaginatedMov
 	return movies, &presence, nil
 }
 
-func (m *MediaDiscovery) SearchShow(query string, page int) (*tmdb.PaginatedTVShowResults, *[]bool, error) {
+func (m *MediaDiscovery) searchAvailableMovie(query string, page int) (*tmdb.PaginatedMovieResults, *[]bool, error) {
+	movies, total, err := m.mediaRepository.SearchMovies(page, 20, query)
+	if err != nil {
+		return nil, nil, err
+	}
+	presence := make([]bool, len(movies))
+	results := make([]*tmdb.Movie, len(movies))
+	locker := make([]sync.Mutex, len(movies))
+	wg := sync.WaitGroup{}
+	for i, movie := range movies {
+		wg.Add(1)
+		go func(i int, movie repository2.Movie) {
+			defer wg.Done()
+			result, err := m.mediaClient.GetMovieShort(movie.MediaID)
+			if err != nil {
+				log.Println("error getting movie", movie.MediaID, err)
+				return
+			}
+			voteAverage, voteCount, err := m.mediaRepository.GetMediaRating(movie.MediaID)
+			if err == nil {
+				result.VoteAverage = voteAverage
+				result.VoteCount = voteCount
+			}
+			locker[i].Lock()
+			defer locker[i].Unlock()
+			results[i] = result
+			presence[i] = true
+		}(i, movie)
+	}
+	wg.Wait()
+	return &tmdb.PaginatedMovieResults{
+		Results:     results,
+		TotalResult: total,
+		TotalPage:   int(math.Round(float64(total) / 20)),
+	}, &presence, nil
+}
+
+func (m *MediaDiscovery) SearchShow(query string, page int, available bool) (*tmdb.PaginatedTVShowResults, *[]bool, error) {
 	shows, err := m.mediaClient.SearchTVShows(query, page)
 	presence := make([]bool, len(shows.Results))
 	if err != nil {
@@ -51,7 +100,7 @@ func (m *MediaDiscovery) SearchShow(query string, page int) (*tmdb.PaginatedTVSh
 	return shows, &presence, nil
 }
 
-func (m *MediaDiscovery) GetPopularMovies(page int) (*tmdb.PaginatedMovieResults, *[]bool, error) {
+func (m *MediaDiscovery) GetPopularMovies(page int, available bool) (*tmdb.PaginatedMovieResults, *[]bool, error) {
 	movies, err := m.mediaClient.GetPopularMovies(page)
 	presence := make([]bool, len(movies.Results))
 	if err != nil {
@@ -68,7 +117,7 @@ func (m *MediaDiscovery) GetPopularMovies(page int) (*tmdb.PaginatedMovieResults
 	return movies, &presence, nil
 }
 
-func (m *MediaDiscovery) GetPopularShows(page int) (*tmdb.PaginatedTVShowResults, *[]bool, error) {
+func (m *MediaDiscovery) GetPopularShows(page int, available bool) (*tmdb.PaginatedTVShowResults, *[]bool, error) {
 	shows, err := m.mediaClient.GetPopularTVShows(page)
 	presence := make([]bool, len(shows.Results))
 	if err != nil {
@@ -85,7 +134,7 @@ func (m *MediaDiscovery) GetPopularShows(page int) (*tmdb.PaginatedTVShowResults
 	return shows, &presence, nil
 }
 
-func (m *MediaDiscovery) GetRecentMovies() ([]*tmdb.Movie, *[]bool, error) {
+func (m *MediaDiscovery) GetRecentMovies(available bool) ([]*tmdb.Movie, *[]bool, error) {
 	movies, err := m.mediaClient.GetRecentMovies()
 	presence := make([]bool, len(movies))
 	if err != nil {
@@ -102,7 +151,7 @@ func (m *MediaDiscovery) GetRecentMovies() ([]*tmdb.Movie, *[]bool, error) {
 	return movies, &presence, nil
 }
 
-func (m *MediaDiscovery) GetRecentShows() ([]*tmdb.TVShow, *[]bool, error) {
+func (m *MediaDiscovery) GetRecentShows(available bool) ([]*tmdb.TVShow, *[]bool, error) {
 	shows, err := m.mediaClient.GetRecentTVShows()
 	presence := make([]bool, len(shows))
 	if err != nil {
