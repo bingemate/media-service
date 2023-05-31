77ccbf722cbe3019b98034c2b03778e0158b8b7e
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-05-31 17:36:41 +0200

    feat: Add error handling for GetUsersMediaRating
    This commit adds error handling to the GetUserMediaRating function in the RatingService struct. If a record is not found, it returns a default rating of 0 instead of an error. This change improves the user experience and prevents unnecessary errors from being thrown.

--- internal/features/rating.go
+++ internal/features/rating.go
@@ -1,8 +1,10 @@
 package features
 
 import (
+	"errors"
 	repository2 "github.com/bingemate/media-go-pkg/repository"
 	"github.com/bingemate/media-service/internal/repository"
+	"gorm.io/gorm"
 )
 
 type RatingService struct {
@@ -22,7 +24,14 @@ func (s *RatingService) GetUsersRating(userID string, page int) ([]*repository2.
 }
 
 func (s *RatingService) GetUserMediaRating(userID string, mediaID int) (*repository2.Rating, error) {
-	return s.mediaRepository.GetUserMediaRating(userID, mediaID)
+	rating, err := s.mediaRepository.GetUserMediaRating(userID, mediaID)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return &repository2.Rating{Rating: 0, UserID: userID, MediaID: mediaID}, nil
+		}
+		return nil, err
+	}
+	return rating, nil
 }
 
 func (s *RatingService) RateMedia(userID string, mediaID, rating int) (*repository2.Rating, error) {
