1227b84828ce4f4f3f5c06601881860dadeac5cd
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-06-09 23:48:17 +0200

    feat: Add endpoints to get movie, episode and available episodes info
    This commit adds new endpoints to retrieve information about movies, episodes and available episodes. The `getMediaFileInfo` function was refactored into `getMovieFileInfo` and `getEpisodeFileInfo`. A new function called `getAvailableEpisodes` was added to retrieve all the available episodes for a TV show. All functions have Swagger documentation.

--- internal/controllers/fileinfo.go
+++ internal/controllers/fileinfo.go
@@ -8,24 +8,30 @@ import (
 )
 
 func InitFileInfoController(engine *gin.RouterGroup, fileInfo *features.MediaFile) {
-	engine.GET("file-tmdb/:id", func(c *gin.Context) {
-		getMediaFileInfo(c, fileInfo)
+	engine.GET("movie/:id", func(c *gin.Context) {
+		getMovieFileInfo(c, fileInfo)
+	})
+	engine.GET("episode/:id", func(c *gin.Context) {
+		getEpisodeFileInfo(c, fileInfo)
+	})
+	engine.GET("tv/:id/available", func(c *gin.Context) {
+		getAvailableEpisodes(c, fileInfo)
 	})
 }
 
-// @Summary		Get media file info by its Media TMDB ID
-// @Description	Get media file info by its Media TMDB ID
-// @Tags			Media File
+// @Summary		Get movie file info by its Movie TMDB ID
+// @Description	Get movie file info by its Movie TMDB ID
+// @Tags			File
 // @Param			id path int true "TMDB ID"
 // @Produce		json
 // @Success		200	{object} mediaFileResponse
 // @Failure		400	{object} errorResponse
 // @Failure		404	{object} errorResponse
 // @Failure		500	{object} errorResponse
-// @Router			/media-file/file-tmdb/{id} [get]
-func getMediaFileInfo(c *gin.Context, mediaData *features.MediaFile) {
+// @Router			/media-file/movie/{id} [get]
+func getMovieFileInfo(c *gin.Context, mediaData *features.MediaFile) {
 	mediaID, err := strconv.Atoi(c.Param("id"))
-	result, err := mediaData.GetMediaFileInfo(mediaID)
+	result, err := mediaData.GetMovieFileInfo(mediaID)
 	if err != nil {
 		if errors.Is(err, features.ErrMediaNotFound) {
 			c.JSON(404, errorResponse{
@@ -45,4 +51,72 @@ func getMediaFileInfo(c *gin.Context, mediaData *features.MediaFile) {
 		return
 	}
 	c.JSON(200, toMediaFileResponse(result))
+}
+
+// @Summary		Get episode file info by its Episode TMDB ID
+// @Description	Get episode file info by its Episode TMDB ID
+// @Tags			File
+// @Param			id path int true "TMDB ID"
+// @Produce		json
+// @Success		200	{object} mediaFileResponse
+// @Failure		400	{object} errorResponse
+// @Failure		404	{object} errorResponse
+// @Failure		500	{object} errorResponse
+// @Router			/media-file/episode/{id} [get]
+func getEpisodeFileInfo(c *gin.Context, mediaData *features.MediaFile) {
+	mediaID, err := strconv.Atoi(c.Param("id"))
+	result, err := mediaData.GetEpisodeFileInfo(mediaID)
+	if err != nil {
+		if errors.Is(err, features.ErrMediaNotFound) {
+			c.JSON(404, errorResponse{
+				Error: err.Error(),
+			})
+			return
+		}
+		if errors.Is(err, features.ErrInvalidMediaType) {
+			c.JSON(400, errorResponse{
+				Error: err.Error(),
+			})
+			return
+		}
+		c.JSON(500, errorResponse{
+			Error: err.Error(),
+		})
+		return
+	}
+	c.JSON(200, toMediaFileResponse(result))
+}
+
+// @Summary Get tv show available episodes
+// @Description Get all available episodes id for a tv show
+// @Tags File
+// @Param id path int true "TMDB ID"
+// @Produce json
+// @Success 200 {array} int
+// @Failure 400 {object} errorResponse
+// @Failure 404 {object} errorResponse
+// @Failure 500 {object} errorResponse
+// @Router /media-file/tv/{id}/available [get]
+func getAvailableEpisodes(c *gin.Context, mediaData *features.MediaFile) {
+	mediaID, err := strconv.Atoi(c.Param("id"))
+	result, err := mediaData.GetAvailableEpisode(mediaID)
+	if err != nil {
+		if errors.Is(err, features.ErrMediaNotFound) {
+			c.JSON(404, errorResponse{
+				Error: err.Error(),
+			})
+			return
+		}
+		if errors.Is(err, features.ErrInvalidMediaType) {
+			c.JSON(400, errorResponse{
+				Error: err.Error(),
+			})
+			return
+		}
+		c.JSON(500, errorResponse{
+			Error: err.Error(),
+		})
+		return
+	}
+	c.JSON(200, result)
 }
