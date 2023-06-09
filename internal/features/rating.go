5814b892fe373227962c24e98922599634ea8c8d
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-06-09 23:45:44 +0200

    feat(rating): Add functions to get and rate movies and TV shows
    This commit adds new functions to the RatingService struct that allow users to get ratings for movies and TV shows, as well as rate them. The previously existing functions for getting user ratings have also been split into separate movie and TV show rating functions.

--- internal/features/rating.go
+++ internal/features/rating.go
@@ -15,25 +15,71 @@ func NewRatingService(mediaRepository *repository.MediaRepository) *RatingServic
 	return &RatingService{mediaRepository}
 }
 
-func (s *RatingService) GetMediaRating(mediaID, page int) ([]*repository2.Rating, int, error) {
-	return s.mediaRepository.GetMediaRatings(mediaID, 10, page)
+//func (s *RatingService) GetMediaRating(mediaID, page int) ([]*repository2.Rating, int, error) {
+//	return s.mediaRepository.GetMediaRatings(mediaID, 10, page)
+//}
+
+func (s *RatingService) GetMovieRatings(movieID, page int) ([]*repository2.MovieRating, int, error) {
+	return s.mediaRepository.GetMovieRatings(movieID, 10, page)
 }
 
-func (s *RatingService) GetUsersRating(userID string, page int) ([]*repository2.Rating, int, error) {
-	return s.mediaRepository.GetUserRatings(userID, 10, page)
+func (s *RatingService) GetTvShowRatings(tvShowID, page int) ([]*repository2.TvShowRating, int, error) {
+	return s.mediaRepository.GetTvShowRatings(tvShowID, 10, page)
 }
 
-func (s *RatingService) GetUserMediaRating(userID string, mediaID int) (*repository2.Rating, error) {
-	rating, err := s.mediaRepository.GetUserMediaRating(userID, mediaID)
+//func (s *RatingService) GetUsersRating(userID string, page int) ([]*repository2.Rating, int, error) {
+//	return s.mediaRepository.GetUserRatings(userID, 10, page)
+//}
+
+func (s *RatingService) GetUsersMovieRatings(userID string, page int) ([]*repository2.MovieRating, int, error) {
+	return s.mediaRepository.GetUserMovieRatings(userID, 10, page)
+}
+
+func (s *RatingService) GetUsersTvShowRatings(userID string, page int) ([]*repository2.TvShowRating, int, error) {
+	return s.mediaRepository.GetUserTvShowRatings(userID, 10, page)
+}
+
+//func (s *RatingService) GetUserMediaRating(userID string, mediaID int) (*repository2.Rating, error) {
+//	rating, err := s.mediaRepository.GetUserMediaRating(userID, mediaID)
+//	if err != nil {
+//		if errors.Is(err, gorm.ErrRecordNotFound) {
+//			return &repository2.Rating{Rating: 0, UserID: userID, MediaID: mediaID}, nil
+//		}
+//		return nil, err
+//	}
+//	return rating, nil
+//}
+
+func (s *RatingService) GetUserMovieRating(userID string, movieID int) (*repository2.MovieRating, error) {
+	rating, err := s.mediaRepository.GetUserMovieRating(userID, movieID)
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return &repository2.Rating{Rating: 0, UserID: userID, MediaID: mediaID}, nil
+			return &repository2.MovieRating{Rating: 0, UserID: userID, MovieID: movieID}, nil
 		}
 		return nil, err
 	}
 	return rating, nil
 }
 
-func (s *RatingService) RateMedia(userID string, mediaID, rating int) (*repository2.Rating, error) {
-	return s.mediaRepository.SaveMediaRating(mediaID, userID, rating)
+func (s *RatingService) GetUserTvShowRating(userID string, tvShowID int) (*repository2.TvShowRating, error) {
+	rating, err := s.mediaRepository.GetUserTvShowRating(userID, tvShowID)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return &repository2.TvShowRating{Rating: 0, UserID: userID, TvShowID: tvShowID}, nil
+		}
+		return nil, err
+	}
+	return rating, nil
+}
+
+//func (s *RatingService) RateMedia(userID string, mediaID, rating int) (*repository2.Rating, error) {
+//	return s.mediaRepository.SaveMediaRating(mediaID, userID, rating)
+//}
+
+func (s *RatingService) RateMovie(userID string, movieID, rating int) (*repository2.MovieRating, error) {
+	return s.mediaRepository.SaveMovieRating(movieID, userID, rating)
+}
+
+func (s *RatingService) RateTvShow(userID string, tvShowID, rating int) (*repository2.TvShowRating, error) {
+	return s.mediaRepository.SaveTvShowRating(tvShowID, userID, rating)
 }
