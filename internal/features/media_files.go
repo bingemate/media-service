9b0e8abe7f88af621b254c09e14b72b3f209549f
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-06-17 17:39:18 +0200

    feat(media_files): improve DeleteMediaFile function
    This commit removes a call to the media repository's DeleteMediaFile
    function and instead calls it at the end of the function. It also adds
    error handling for deleting folders in case of an error.

--- internal/features/media_files.go
+++ internal/features/media_files.go
@@ -94,10 +94,6 @@ func (m *MediaFile) SearchMovieFiles(query string, page, limit int) ([]*reposito
 
 // DeleteMediaFile deletes a media file given the fileID
 func (m *MediaFile) DeleteMediaFile(fileID string) error {
-	err := m.mediaRepository.DeleteMediaFile(fileID)
-	if err != nil {
-		return err
-	}
 	// Check if the file is present in movie or tv show folder
 	// If it is, delete it
 	episode, err := m.mediaRepository.GetEpisodeByFileID(fileID)
@@ -107,7 +103,10 @@ func (m *MediaFile) DeleteMediaFile(fileID string) error {
 	if episode != nil {
 		tvPath := path.Join(m.tvPath, strconv.Itoa(episode.ID))
 		log.Println("Deleting folder: ", tvPath)
-		return os.RemoveAll(tvPath)
+		err = os.RemoveAll(tvPath)
+		if err != nil {
+			return err
+		}
 	}
 
 	movie, err := m.mediaRepository.GetMovieByFileID(fileID)
@@ -117,10 +116,12 @@ func (m *MediaFile) DeleteMediaFile(fileID string) error {
 	if movie != nil {
 		moviePath := path.Join(m.moviePath, strconv.Itoa(movie.ID))
 		log.Println("Deleting folder: ", moviePath)
-		return os.RemoveAll(moviePath)
+		err = os.RemoveAll(moviePath)
+		if err != nil {
+			return err
+		}
 	}
-	return errors.New("file not found")
-	
+	return m.mediaRepository.DeleteMediaFile(fileID)
 }
 
 // MediaFilesTotalSize returns the total size of all media files
