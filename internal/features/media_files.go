0d9cbf638bf22400e6606d419b1c86252afe5f9f
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-06-17 16:40:24 +0200

    feat(media_files): Add functionality to delete media files from movie and tv show folders
    This commit adds the ability to delete media files from both movie and tv show folders. When a file is deleted, the code checks if it exists in either folder and removes it if present. The commit also includes an import for the "path" package.

--- internal/features/media_files.go
+++ internal/features/media_files.go
@@ -5,6 +5,8 @@ import (
 	repository2 "github.com/bingemate/media-go-pkg/repository"
 	"github.com/bingemate/media-service/internal/repository"
 	"gorm.io/gorm"
+	"os"
+	"path"
 	"syscall"
 )
 
@@ -90,7 +92,29 @@ func (m *MediaFile) SearchMovieFiles(query string, page, limit int) ([]*reposito
 
 // DeleteMediaFile deletes a media file given the fileID
 func (m *MediaFile) DeleteMediaFile(fileID string) error {
-	return m.mediaRepository.DeleteMediaFile(fileID)
+	err := m.mediaRepository.DeleteMediaFile(fileID)
+	if err != nil {
+		return err
+	}
+	// Check if the file is present in movie or tv show folder
+	// If it is, delete it
+	moviePath := path.Join(m.moviePath, fileID)
+	tvPath := path.Join(m.tvPath, fileID)
+	_, err = os.Stat(moviePath)
+	if err == nil {
+		err = os.RemoveAll(moviePath)
+		if err != nil {
+			return err
+		}
+	}
+	_, err = os.Stat(tvPath)
+	if err == nil {
+		err = os.RemoveAll(tvPath)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 // MediaFilesTotalSize returns the total size of all media files
