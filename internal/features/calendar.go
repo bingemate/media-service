7e34a94c4c64f74f76d1a031794370464b99d529
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-06-24 14:09:36 +0200

    feat(calendar): add range functionality to GetMoviesCalendar and GetTvShowCalendar
    This commit adds the ability to get movie and TV show releases within a specified date range. The `GetMoviesCalendar` and `GetTvShowCalendar` functions now take an additional parameter for the year, allowing users to specify a specific month and year. Two new functions, `GetMoviesCalendarInRange` and `GetTvShowCalendarInRange`, have been added to retrieve releases within a given time frame.

--- internal/features/calendar.go
+++ internal/features/calendar.go
@@ -16,8 +16,8 @@ func NewCalendarService(mediaClient tmdb.MediaClient, mediaRepository *repositor
 	return &CalendarService{mediaClient, mediaRepository}
 }
 
-func (s *CalendarService) GetMoviesCalendar(userID string, month int) ([]*tmdb.Movie, *[]bool, error) {
-	startOfMonth := time.Date(time.Now().Year(), time.Month(month), 1, 0, 0, 0, 0, time.UTC)
+func (s *CalendarService) GetMoviesCalendar(userID string, month int, year int) ([]*tmdb.Movie, *[]bool, error) {
+	startOfMonth := time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.UTC)
 	endOfMonth := startOfMonth.AddDate(0, 1, 0)
 	followedReleases, err := s.mediaRepository.GetFollowedMoviesReleases(userID)
 	if err != nil {
@@ -36,14 +36,50 @@ func (s *CalendarService) GetMoviesCalendar(userID string, month int) ([]*tmdb.M
 	return movies, &presence, nil
 }
 
-func (s *CalendarService) GetTvShowCalendar(userID string, month int) ([]*tmdb.TVEpisode, []*tmdb.TVShow, *[]bool, error) {
-	startOfMonth := time.Date(time.Now().Year(), time.Month(month), 1, 0, 0, 0, 0, time.UTC)
+func (s *CalendarService) GetTvShowCalendar(userID string, month int, year int) ([]*tmdb.TVEpisode, []*tmdb.TVShow, *[]bool, error) {
+	startOfMonth := time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.UTC)
 	endOfMonth := startOfMonth.AddDate(0, 1, 0)
 	followedReleases, err := s.mediaRepository.GetFollowedTvShowsReleases(userID)
 	if err != nil {
 		return nil, nil, nil, err
 	}
 	episodes, tvShows, err := s.mediaClient.GetTVShowsReleases(*followedReleases, startOfMonth, endOfMonth)
+	if err != nil {
+		log.Println(err)
+		return nil, nil, nil, err
+	}
+	presence := make([]bool, len(episodes))
+	for i, episode := range episodes {
+		presence[i] = s.mediaRepository.IsEpisodeFilePresent(episode.ID)
+	}
+
+	return episodes, tvShows, &presence, nil
+}
+
+func (s *CalendarService) GetMoviesCalendarInRange(userID string, start time.Time, end time.Time) ([]*tmdb.Movie, *[]bool, error) {
+	followedReleases, err := s.mediaRepository.GetFollowedMoviesReleases(userID)
+	if err != nil {
+		return nil, nil, err
+	}
+	var presence []bool
+	movies, err := s.mediaClient.GetMoviesReleases(*followedReleases, start, end)
+	if err != nil {
+		log.Println(err)
+		return nil, nil, err
+	}
+	presence = make([]bool, len(movies))
+	for i, movie := range movies {
+		presence[i] = s.mediaRepository.IsMovieFilePresent(movie.ID)
+	}
+	return movies, &presence, nil
+}
+
+func (s *CalendarService) GetTvShowCalendarInRange(userID string, start time.Time, end time.Time) ([]*tmdb.TVEpisode, []*tmdb.TVShow, *[]bool, error) {
+	followedReleases, err := s.mediaRepository.GetFollowedTvShowsReleases(userID)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	episodes, tvShows, err := s.mediaClient.GetTVShowsReleases(*followedReleases, start, end)
 	if err != nil {
 		log.Println(err)
 		return nil, nil, nil, err
